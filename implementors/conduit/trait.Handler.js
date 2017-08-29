594340efc9be187a68f4a5cb23e4adabee69a007
Travis CI User <travis@example.org>
2017-08-29 10:07:12 +0000

    Documentation for conduit-rust/conduit-middleware@77f673e

---
+++ implementors/conduit/trait.Handler.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["conduit_middleware"] = ["impl Handler for <a class=\"struct\" href=\"conduit_middleware/struct.MiddlewareBuilder.html\" title=\"struct conduit_middleware::MiddlewareBuilder\">MiddlewareBuilder</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
